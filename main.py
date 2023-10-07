fe10080fb76a12f59310d7d11ead871a94a8f127
niccoarch <114556651+NiccoArcos@users.noreply.github.com>
2023-10-06 21:53:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,8 @@ class HiloConexion(QObject):
     def run(self):
         signal = 1
         if Conexion._conexion is None:
-            print('Estableciendo conexion...')
             Conexion.conexion()
             Envio.envio()
-
 
 
 
@@ -46,30 +44,42 @@ class VentanaPrincipal(QMainWindow,Ui_MainWindow):
         #Boton Menu control de eventos
         self.boton_menu.setCheckable(True)
 #        self.boton_menu.clicked.connect(self.evento_checar)
+#
         self.boton_menu.clicked.connect(self.mover_menu)
 
         #Boton Prueba Diaria control de eventos
         self.boton1.setCheckable(True)
         self.boton1.clicked.connect(self.evento_checar_diaria)
-        self.boton1.clicked.connect(self.mover_prueba_diaria)
+        self.boton1.clicked.connect(self.mover_cabecera_custom_1)
+
+
 
         #Boton prueba general
-        prueba = False
 
-        self.boton_gral.setCheckable(True)
-        self.boton_gral.clicked.connect(self.mover_widet7)
-        self.conexion_label.setText('Conexion establecida')
-        self.envio_msj.setText(f'Mensaje enviado con exito a:{Envio.destino}')
-        self.sesion_cerrada_label.setText(f'Sesion cerrada')
+        self.btn_gral_2.setCheckable(True)
+        self.btn_gral_2.clicked.connect(self.evento_checar_btn_gral_2)
+        self.btn_gral_2.clicked.connect(self.mover_columnas_custom_1)
+
+        self.btn_particular_prt_2.setCheckable(True)
+        self.btn_particular_prt_2.clicked.connect(self.evento_checar_btn_particular_2)
+
+
+        #self.boton_particular.clicked.connect(self.mover_widet7)
+        #Retomar desde aca para programar las labels de las pruebas
+        # if Conexion.conexione is not None:
+        #     self.conexion_label.setText(f'Conexion establecida con exito a: {Conexion.username}')
+        # #self.conexion_label.setText('Conexion establecida')
+        # self.envio_msj.setText(f'Mensaje enviado con exito a:{Envio.destino}')
+        # self.sesion_cerrada_label.setText(f'Sesion cerrada')
+
+
 
 
         #HILO DE CONEXION Y ENVIO
         self.thread = QThread()
         self.worker = HiloConexion()
-
         self.worker.moveToThread(self.thread)
-
-        self.boton_gral.clicked.connect(self.worker.run)
+        #self.boton_gral.clicked.connect(self.worker.run)
         #self.worker.finished.connect(self.thread.quit)
 #       self.worker.finished.connect(self.worker.deleteLater)
         self.thread.finished.connect(self.thread.deleteLater)
@@ -86,30 +96,28 @@ class VentanaPrincipal(QMainWindow,Ui_MainWindow):
         self.boton_diaria_checado = checar_diaria
         print(f'Boton diaria checado',self.boton_diaria_checado)
 
+    def evento_checar_btn_gral_2(self,checar_general_2):
+        self.boton_general_2 = checar_general_2
+        print(f'Boton General checado',self.boton_general_2)
+
+    def evento_checar_btn_particular_2(self,checar_btn_particular_2):
+        self.boton_particular_2 = checar_btn_particular_2
+        print(f'Boton particular checado',self.boton_particular_2)
 
     def mover_menu(self):
         self.width = self.menu_lateral.width()
 
         if self.width == 0:
             #ExpandMenu
-            self.newWidth=147
+            self.newWidth=900
         else:
             self.newWidth=0
 
 
-        #Vincular la animacion de contraccion del menu general con los botones de prueba
-        self.newHeight = 0
-        self.animacion = QPropertyAnimation(self.botones_pruebas, b"maximumHeight")
-        self.animacion.setDuration(700)
-        self.animacion.setStartValue(self.newHeight)
-        self.animacion.setEndValue(self.width)
-        self.animacion.setEasingCurve(QEasingCurve.InOutQuart)
-        self.animacion.start()
-
-        #Vincular animacion de boton menu con menu lateral
+    # Vincular animacion de boton menu con menu lateral
         self.animacion = QPropertyAnimation(self.menu_lateral, b"maximumWidth")
-        # Animate minumWidth
-        self.animacion.setDuration(500)
+    # Animate minumWidth
+        self.animacion.setDuration(900)
         self.animacion.setStartValue(self.width)
         self.animacion.setEndValue(self.newWidth)
         self.animacion.setEasingCurve(QEasingCurve.InOutQuart)
@@ -117,54 +125,78 @@ class VentanaPrincipal(QMainWindow,Ui_MainWindow):
 
 
 
-    def mover_prueba_diaria(self):
-        self.height = self.botones_pruebas.height()
-
-        if self.height == 0:
-            # ExpandirMenu
-            self.newHeight = 75
+    def mover_cabecera_custom_1(self):
+        self.width = self.frame_btns_cabecera_custom_1.width()
+        if self.width == 0:
+            self.newWidth = 1000
 
         else:
-            self.newHeight = 0
+            self.newWidth = 0
 
-
-        #Animacion botones de prueba diaria (prueba general, particular)
-        self.animacion = QPropertyAnimation(self.botones_pruebas, b"maximumHeight")
-        # Animate minumWidth
-        self.animacion.setDuration(500)
+        self.newHeight = 0
+        self.height = 0
+        self.animacion = QPropertyAnimation(self.columnas_custom_1, b"maximumHeight")
+        self.animacion.setDuration(900)
         self.animacion.setStartValue(self.height)
         self.animacion.setEndValue(self.newHeight)
         self.animacion.setEasingCurve(QEasingCurve.InOutQuart)
         self.animacion.start()
 
-
-    def mover_widet7(self):
-        self.width_widget7 = self.widget_7.width()
-        if self.width_widget7 == 0:
-            self.newWidth_wiget7 = 500
-
-        else:
-            self.newWidth_wiget7 = 0
-
-        self.animacion = QPropertyAnimation(self.widget_7, b"minimumWidth")
-        self.animacion.setDuration(500)
-        self.animacion.setStartValue(self.width_widget7)
-        self.animacion.setEndValue(self.newWidth_wiget7)
+        self.animacion = QPropertyAnimation(self.frame_btns_cabecera_custom_1, b"maximumWidth")
+        self.animacion.setDuration(900)
+        self.animacion.setStartValue(self.width)
+        self.animacion.setEndValue(self.newWidth)
         self.animacion.setEasingCurve(QEasingCurve.InOutQuart)
         self.animacion.start()
 
 
 
-        self.boton1.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page))
 
-        # PAGINA 2
-        self.boton2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_2))
 
-        # PAGINA 3
-        self.boton3.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_3))
+    def mover_columnas_custom_1(self):
+        self.height = self.columnas_custom_1.height()
+        if self.height == 0:
+            self.newHeight = 1000
+        else:
+            self.newHeight = 0
+        self.animacion = QPropertyAnimation(self.columnas_custom_1, b"maximumHeight")
+        self.animacion.setDuration(900)
+        self.animacion.setStartValue(self.height)
+        self.animacion.setEndValue(self.newHeight)
+        self.animacion.setEasingCurve(QEasingCurve.InOutQuart)
+        self.animacion.start()
+
+    #
+    #
+    #
+    #
+    #
+    #
+    # def mover_colum_btns_ejec(self):
+    #     self.height = self.frame_colum_btns_ejec.height()
+    #     if self.height == 0:
+    #         self.newHeight= 1000
+    #     else:
+    #         self.newHeight = 0
+    #
+    #
+    #     self.animacion = QPropertyAnimation(self.frame_colum_btns_ejec, b"maximumHeight")
+    #     self.animacion.setDuration(900)
+    #     self.animacion.setStartValue(self.height)
+    #     self.animacion.setEndValue(self.newHeight)
+    #     self.animacion.setEasingCurve(QEasingCurve.InOutQuart)
+    #     self.animacion.start()
+
+
+        # PAGINA 1
+        self.boton1.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.pagina1))
+
+        self.btn_gral_3.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.pagina1))
+
+        self.btn_particular_prt_2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.pagina2))
 
         # PAGINA 4
-        self.boton4.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_4))
+        self.boton4.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.pagina4))
 
 
 
